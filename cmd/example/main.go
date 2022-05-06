c560f411e9fdc0aae8fc8d85609a5e9685abde58
yaroslav_els <yarter003@gmail.com>
2022-05-07 01:25:50 +0300

    implemented handler and its usage

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -1,5 +1,48 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"os"
+	"strings"
+	lab2 "github.com/Rabotiagi/SA_Lab2"
+)
 
+var (
+	inputExpression = flag.String("e", "", "Expression to compute")
+	inputDirectory = flag.String("f", "", "Directory of the expression to compute")
+	outputDirectory = flag.String("o", "", "Directory to place the output in")
+)
+
+func main() {
+	flag.Parse()
+
+	if *inputExpression != "" && *inputDirectory != "" {
+		panic("only one flag can be used for input specifying")
+	}
+
+	handler := &lab2.ComputeHandler {
+		Input: strings.NewReader(*inputExpression),
+		Output: os.Stdout,
+	}
+
+	if *inputDirectory != "" {
+		handlerInput, err1 := os.OpenFile(*inputDirectory, 0, 0777)
+		if (err1 != nil) {
+			panic(err1)
+		}
+		handler.Input = handlerInput
+	}
+
+	if *outputDirectory != "" {
+		handlerOutput, err2 := os.OpenFile(*outputDirectory, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0777)
+		if (err2 != nil) {
+			panic(err2)
+		}
+		handler.Output = handlerOutput
+	}
+
+	err := handler.Compute()
+	if (err != nil) {
+		panic(err)
+	}
 }
--- handler.go
+++ handler.go
@@ -1,8 +1,32 @@
 package lab2
 
+import (
+	"fmt"
+	"io"
+)
+
 type ComputeHandler struct {
+	Input io.Reader
+	Output io.Writer
 }
 
+
 func (ch *ComputeHandler) Compute() error {
+	data := make([]byte, 1024)
+	count, err1 := ch.Input.Read(data)
+	if err1 != nil {
+		return err1
+	}
+
+	res, err2 := PostfixToInfix(string(data[:count]))
+	if err2 != nil {
+		return fmt.Errorf("invalid input")
+	}
+
+	_, err3 := ch.Output.Write([]byte(res + "\n"))
+	if err3 != nil {
+		return err3
+	}
+
 	return nil
 }
